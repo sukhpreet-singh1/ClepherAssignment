0039372dfd4c3e2578ddb42ae161e3f0d769275a
sukhpreet-singh1 <493_bt19@iiitkalyani.ac.in>
2024-07-08 22:40:44 +0530

    Changes

--- src/DataFetcher.tsx
+++ src/DataFetcher.tsx
@@ -3,24 +3,27 @@ import StockChart from "./StockChart";
 import Header from "./Header";
 import Dropdown from "./Dropdown";
 
+interface MetaData {
+  "1. Information": string;
+  "2. Symbol": string;
+  "3. Last Refreshed": string;
+  "4. Interval": string | null;
+  "4. Output Size": string;
+  "5. Time Zone": string;
+}
+
+interface TimeSeriesData {
+  "1. open": string;
+  "2. high": string;
+  "3. low": string;
+  "4. close": string;
+  "5. volume": string;
+}
+
 interface StockData {
-  "Meta Data": {
-    "1. Information": string;
-    "2. Symbol": string;
-    "3. Last Refreshed": string;
-    "4. Interval": string;
-    "5. Output Size": string;
-    "6. Time Zone": string;
-  };
-  "Time Series (5min)": {
-    [key: string]: {
-      "1. open": string;
-      "2. high": string;
-      "3. low": string;
-      "4. close": string;
-      "5. volume": string;
-    };
-  };
+  "Meta Data": MetaData;
+  "Time Series (5min)"?: { [key: string]: TimeSeriesData };
+  "Time Series (Daily)"?: { [key: string]: TimeSeriesData };
 }
 
 const DataFetcher: React.FC = () => {
@@ -59,17 +62,22 @@ const DataFetcher: React.FC = () => {
     };
 
     fetchData();
-  }, []);
+  }, [selectedDropdown]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
   if (!stockData) return <div>No data available</div>;
 
+  const timeSeriesData =
+    selectedDropdown === "TIME_SERIES_INTRADAY"
+      ? stockData["Time Series (5min)"]
+      : stockData["Time Series (Daily)"];
+
   return (
     <>
       <Header data={stockData["Meta Data"]} />
       <Dropdown options={dropdownOptions} onChange={handleDropdownChange} />
-      <StockChart data={stockData["Time Series (5min)"]} />
+      <StockChart data={timeSeriesData} />
     </>
   );
 };
